59e660eb46781fc6093aaa3618787d12aa605a8e
Your Name <you@example.com>
2025-01-04 19:21:31 +0700

    Update add Google Sign In

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,16 @@
-import { Image, StyleSheet, Dimensions, View } from "react-native";
-
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Dimensions,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import {
+  GoogleSignin,
+  SignInSuccessResponse,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
@@ -8,6 +19,64 @@ import { ThemedView } from "@/components/ThemedView";
 const { width: screenWidth } = Dimensions.get("window");
 
 export default function HomeScreen() {
+  const [user, setUser] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "38421602592-cafkdnuah5tnokb5lnoe7b5rlv61pq1d.apps.googleusercontent.com",
+    });
+
+    const checkUserAuth = async () => {
+      try {
+        const currentUser = await auth().currentUser;
+
+        if (currentUser) {
+          setUser(currentUser);
+        } else {
+          await signInWithGoogle();
+        }
+      } catch (error) {
+        console.error("Gagal memeriksa autentikasi:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkUserAuth();
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const signInResult = await GoogleSignin.signIn();
+
+      // Akses idToken dari signInResult.data
+      const idToken = signInResult?.data?.idToken;
+
+      if (!idToken) {
+        throw new Error("idToken tidak ditemukan");
+      }
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      await auth().signInWithCredential(googleCredential);
+
+      setUser(auth().currentUser);
+    } catch (error) {
+      console.error("Gagal login dengan Google:", error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -66,6 +135,11 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   titleContainer: {
     flexDirection: "row",
     alignItems: "center",
